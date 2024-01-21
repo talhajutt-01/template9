8c5f259771978add5e604953c070c9460292e9a9
talhajutt-01 <talhaj928@gmail.com>
2024-01-21 17:14:51 +0500

    commit

--- js/script.js
+++ js/script.js
@@ -122,102 +122,112 @@ function updateBackgroundColor(element, newColor) {
   // Fetch JSON data from the text file
   function updatePageContent(pageName) {
     console.log('Updating content for page:', pageName);
-    fetch('t.txt')
-        .then(response => response.json())
-        .then(jsonData => {
+    const fetchJson1 = fetch('t.txt')
+    .then(response => response.json());
+
+// Fetch the second JSON file
+const fetchJson2 = fetch('image.txt')
+    .then(response => response.json());
+
+// Wait for both fetch operations to complete
+Promise.all([fetchJson1, fetchJson2])
+    .then(jsonDataArray => {
+        // Merge the data from both JSON files
+        const mergedData = Object.assign({}, ...jsonDataArray);
+
             // Update content based on the current page
-            updateContent(title, jsonData.title.title);
+            updateContent(title, mergedData.title.title);
 
-            updateLinkContent(nav0, jsonData.nav.nav0);
-            updateLinkContent(nav1, jsonData.nav.nav1);
-            updateLinkContent(nav2, jsonData.nav.nav2);
-            updateLinkContent(nav3, jsonData.nav.nav3);
-            updateLinkContent(nav4, jsonData.nav.nav4);
+            updateLinkContent(nav0, mergedData.nav.nav0);
+            updateLinkContent(nav1, mergedData.nav.nav1);
+            updateLinkContent(nav2, mergedData.nav.nav2);
+            updateLinkContent(nav3, mergedData.nav.nav3);
+            updateLinkContent(nav4, mergedData.nav.nav4);
             
-            updateContent(b0, jsonData.banner.b0);
-            updateContent(b1, jsonData.banner.b1);
-            updateContent(b2, jsonData.banner.b2);
-            updateContent(b3, jsonData.banner.b3);
-            updateContent(b4, jsonData.banner.b4);
-            updateContent(b5, jsonData.banner.b5);
-            updateContent(b6, jsonData.banner.b6);
-            updateContent(b7, jsonData.banner.b7);
-            updateContent(b8, jsonData.banner.b8);
+            updateContent(b0, mergedData.banner.b0);
+            updateContent(b1, mergedData.banner.b1);
+            updateContent(b2, mergedData.banner.b2);
+            updateContent(b3, mergedData.banner.b3);
+            updateContent(b4, mergedData.banner.b4);
+            updateContent(b5, mergedData.banner.b5);
+            updateContent(b6, mergedData.banner.b6);
+            updateContent(b7, mergedData.banner.b7);
+            updateContent(b8, mergedData.banner.b8);
            
-            updateContent(a0, jsonData.about.a0);
-            updateContent(a1, jsonData.about.a1);
-            updateContent(a2, jsonData.about.a2);
+            updateContent(a0, mergedData.about.a0);
+            updateContent(a1, mergedData.about.a1);
+            updateContent(a2, mergedData.about.a2);
     
-            updateContent(s1, jsonData.service.s1);
-            updateContent(s2, jsonData.service.s2);
-            updateContent(s3, jsonData.service.s3);
-            updateContent(s4, jsonData.service.s4);
-            updateContent(s5, jsonData.service.s5);
-            updateContent(s6, jsonData.service.s6);
-            updateContent(s7, jsonData.service.s7);
-            updateContent(s8, jsonData.service.s8);
-            updateContent(s9, jsonData.service.s9);
-            updateContent(s10, jsonData.service.s10);
-            updateContent(s11, jsonData.service.s11);
-            updateContent(s12, jsonData.service.s12);
+            updateContent(s1, mergedData.service.s1);
+            updateContent(s2, mergedData.service.s2);
+            updateContent(s3, mergedData.service.s3);
+            updateContent(s4, mergedData.service.s4);
+            updateContent(s5, mergedData.service.s5);
+            updateContent(s6, mergedData.service.s6);
+            updateContent(s7, mergedData.service.s7);
+            updateContent(s8, mergedData.service.s8);
+            updateContent(s9, mergedData.service.s9);
+            updateContent(s10, mergedData.service.s10);
+            updateContent(s11, mergedData.service.s11);
+            updateContent(s12, mergedData.service.s12);
 
-            updateContent(bi0, jsonData.bitcoin.bi0);
-            updateContent(bi1, jsonData.bitcoin.bi1);
+            updateContent(bi0, mergedData.bitcoin.bi0);
+            updateContent(bi1, mergedData.bitcoin.bi1);
 
-            updateContent(w0, jsonData.wallet.w0); 
-            updateContent(w1, jsonData.wallet.w1);                      
+            updateContent(w0, mergedData.wallet.w0); 
+            updateContent(w1, mergedData.wallet.w1);                      
 
-            updateContent(d0, jsonData.subscribe.d0);
-            updateContent(d1, jsonData.subscribe.d1);
+            updateContent(d0, mergedData.subscribe.d0);
+            updateContent(d1, mergedData.subscribe.d1);
         
-            updateContent(t0, jsonData.testimonial.t0);
-            updateContent(t1, jsonData.testimonial.t1);
-            updateContent(t2, jsonData.testimonial.t2);
-            updateContent(t3, jsonData.testimonial.t3);
-            updateContent(t4, jsonData.testimonial.t4);
-            updateContent(t5, jsonData.testimonial.t5);
-            updateContent(t6, jsonData.testimonial.t6);
-            updateContent(t7, jsonData.testimonial.t7);
-            updateContent(t8, jsonData.testimonial.t8);
+            updateContent(t0, mergedData.testimonial.t0);
+            updateContent(t1, mergedData.testimonial.t1);
+            updateContent(t2, mergedData.testimonial.t2);
+            updateContent(t3, mergedData.testimonial.t3);
+            updateContent(t4, mergedData.testimonial.t4);
+            updateContent(t5, mergedData.testimonial.t5);
+            updateContent(t6, mergedData.testimonial.t6);
+            updateContent(t7, mergedData.testimonial.t7);
+            updateContent(t8, mergedData.testimonial.t8);
     
-            updateContent(f1, jsonData.footer.f1);
-            updateContent(f2, jsonData.footer.f2);
-            updateContent(f3, jsonData.footer.f3);
-            updateContent(f4, jsonData.footer.f4);
-            updateContent(f5, jsonData.footer.f5);
-            updateContent(f6, jsonData.footer.f6);
-            updateContent(f7, jsonData.footer.f7);
-            updateContent(f8, jsonData.footer.f8);
-            updateContent(f9, jsonData.footer.f9);
-            updateContent(f10, jsonData.footer.f10);
-            updateContent(f11, jsonData.footer.f11);
-            updateContent(f12, jsonData.footer.f12);
-            updateContent(f13, jsonData.footer.f13);
+            updateContent(f1, mergedData.footer.f1);
+            updateContent(f2, mergedData.footer.f2);
+            updateContent(f3, mergedData.footer.f3);
+            updateContent(f4, mergedData.footer.f4);
+            updateContent(f5, mergedData.footer.f5);
+            updateContent(f6, mergedData.footer.f6);
+            updateContent(f7, mergedData.footer.f7);
+            updateContent(f8, mergedData.footer.f8);
+            updateContent(f9, mergedData.footer.f9);
+            updateContent(f10, mergedData.footer.f10);
+            updateContent(f11, mergedData.footer.f11);
+            updateContent(f12, mergedData.footer.f12);
+            updateContent(f13, mergedData.footer.f13);
 
-            updateBackgroundColor(g0, jsonData.color.header);
-            updateBackgroundColor(g1, jsonData.color.about);
-            updateBackgroundColor(g2, jsonData.color.service);
-            updateBackgroundColor(g3, jsonData.color.bitcoin);
-            updateBackgroundColor(g4, jsonData.color.wallet);
-            updateBackgroundColor(g5, jsonData.color.subscribe);
-            updateBackgroundColor(g6, jsonData.color.testimonial);
-            updateBackgroundColor(g7, jsonData.color.contact);
-            updateBackgroundColor(g7, jsonData.color.footer);
+            updateBackgroundColor(g0, mergedData.color.header);
+            updateBackgroundColor(g1, mergedData.color.about);
+            updateBackgroundColor(g2, mergedData.color.service);
+            updateBackgroundColor(g3, mergedData.color.bitcoin);
+            updateBackgroundColor(g4, mergedData.color.wallet);
+            updateBackgroundColor(g5, mergedData.color.subscribe);
+            updateBackgroundColor(g6, mergedData.color.testimonial);
+            updateBackgroundColor(g7, mergedData.color.contact);
+            updateBackgroundColor(g7, mergedData.color.footer);
 
-            updateImageSrc(img1, jsonData.images.img1);
-            updateImageSrc(img2, jsonData.images.img2);
-            updateImageSrc(img3, jsonData.images.img3);
-            updateImageSrc(img4, jsonData.images.img4);
-            updateImageSrc(img5, jsonData.images.img5);
-            updateImageSrc(img6, jsonData.images.img6);
-            updateImageSrc(img7, jsonData.images.img7);
-            updateImageSrc(img8, jsonData.images.img8);
-            updateImageSrc(img9, jsonData.images.img9);
-            updateImageSrc(img10, jsonData.images.img10);
-            updateImageSrc(img11, jsonData.images.img11);
-            updateImageSrc(img12, jsonData.images.img12);
-            updateImageSrc(img13, jsonData.images.img13);
-            updateImageSrc(img14, jsonData.images.img14); 
+            updateImageSrc(img1, mergedData.images.img1);
+            updateImageSrc(img2, mergedData.images.img2);
+            updateImageSrc(img3, mergedData.images.img3);
+            updateImageSrc(img4, mergedData.images.img4);
+            updateImageSrc(img5, mergedData.images.img5);
+            updateImageSrc(img6, mergedData.images.img6);
+            updateImageSrc(img7, mergedData.images.img7);
+            updateImageSrc(img8, mergedData.images.img8);
+            updateImageSrc(img9, mergedData.images.img9);
+            updateImageSrc(img10, mergedData.images.img10);
+            updateImageSrc(img11, mergedData.images.img11);
+            updateImageSrc(img12, mergedData.images.img12);
+            updateImageSrc(img13, mergedData.images.img13);
+            updateImageSrc(img14, mergedData.images.img14); 
             
         
         })
